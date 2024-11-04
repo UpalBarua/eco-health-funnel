31c5bdae03be60113ea7f4700a1fa0af118e196c
UpalBarua <upalbarua52@gmail.com>
2024-11-04 10:18:46 +0600

    add: satisfaction section

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
-import { AsSeenIn } from "./components/as-seen-in";
-import { Banner } from "./components/banner";
-import { Header } from "./components/header";
-import { HealthBenefits } from "./components/health-benefits";
-import { Offerings } from "./components/offerings";
-import { TheProblem } from "./components/the-problem";
-import { TheSolution } from "./components/the-solution";
-import { WhyBeetroot } from "./components/why-beetroot";
-import { HealthyIngredients } from "./components/healthy-ingredients";
+// import { AsSeenIn } from "./components/as-seen-in";
+// import { Banner } from "./components/banner";
+// import { Header } from "./components/header";
+// import { HealthBenefits } from "./components/health-benefits";
+// import { Offerings } from "./components/offerings";
+// import { TheProblem } from "./components/the-problem";
+// import { TheSolution } from "./components/the-solution";
+// import { WhyBeetroot } from "./components/why-beetroot";
+// import { HealthyIngredients } from "./components/healthy-ingredients";
+import { Satisfaction } from "./components/satisfaction";
 
 export function App() {
   return (
@@ -19,7 +20,8 @@ export function App() {
       {/* <TheSolution /> */}
       {/* <WhyBeetroot /> */}
       {/* <HealthBenefits /> */}
-      <HealthyIngredients />
+      {/* <HealthyIngredients /> */}
+      <Satisfaction />
     </div>
   );
 }
---
+++ src/components/satisfaction.tsx
@@ -0,0 +1,34 @@
+import { AddToCartButton } from "./add-to-cart-button";
+import { CartBadge } from "./cart-badge";
+import { SalesBadge } from "./sales-badge";
+
+export function Satisfaction() {
+  return (
+    <section className="relative mx-auto max-w-xl space-y-4 rounded-xl bg-brand-beige p-8 text-center lg:max-w-full">
+      <h3 className="pb-2 text-xl font-bold text-brand-maroon sm:text-2xl">
+        You have nothing to lose!
+      </h3>
+      <p className="text-3xl font-bold sm:text-5xl">
+        100% satisfaction guaranteed on every order!
+      </p>
+      <img src="/images/img-satisfaction.jpg" alt="satisfaction image" />
+      <img
+        className="mx-auto size-20"
+        src="/icons/icon-90-day.png"
+        alt="90 day icon"
+      />
+      <p className="text-lg leading-relaxed">
+        At Snap Supplements we believe optimizing your health should be simple.
+        That’s why we’re committed to providing you with natural, high-quality
+        supplements made with only the purest and carefully selected superfoods,
+        botanicals, vitamins, and minerals that have been scientifically proven
+        to produce real results.
+      </p>
+      <div className="grid justify-items-center gap-y-6">
+        <SalesBadge />
+        <CartBadge />
+        <AddToCartButton />
+      </div>
+    </section>
+  );
+}
