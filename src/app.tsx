b2d8335e90b53cbbc3f2820fadbc657d278294e5
UpalBarua <upalbarua52@gmail.com>
2024-11-09 16:28:00 +0600

    fix ts errors

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import { Banner } from "./components/banner";
 import { CustomerReviews } from "./components/customer-reviews";
 import { Faq } from "./components/faq";
-import { Footer } from "./components/footer";
+// import { Footer } from "./components/footer";
 import { Header } from "./components/header";
 import { HealthBenefits } from "./components/health-benefits";
 import { HealthyIngredients } from "./components/healthy-ingredients";
 import { Packages } from "./components/packages";
-import { Satisfaction } from "./components/satisfaction";
+// import { Satisfaction } from "./components/satisfaction";
 import { TheProblem } from "./components/the-problem";
 import { TheSolution } from "./components/the-solution";
 import { WhyBeetroot } from "./components/why-beetroot";
